523150985095410767c2139613e4b02517056bf6
Rajkumar Parihar <55016311+RajP7Jowa@users.noreply.github.com>
2020-05-02 13:23:38 +0530

    Add files via upload

---
+++ kisansms.php
@@ -0,0 +1,28 @@
+				<div class="row">
+					<a class="col-lg-4 col-md-4 " href="kisan4.pdf" target="_blank">
+        			    <div class="box" >
+              			<span>04</span>
+              			<h4>मई 2020</h4>
+					   </div>
+       		   </a>
+				<a class="col-lg-4 col-md-4 " href="kisan2.pdf" target="_blank">
+        			    <div class="box" >
+              			<span>02</span>
+              			<h4>मई 2020</h4>
+					   </div>
+       		   </a>		
+					<a class="col-lg-4 col-md-4 " href="kisan1.pdf" target="_blank">
+        			    <div class="box" >
+              			<span>01</span>
+              			<h4>मई 2020</h4>
+					   </div>
+       		   </a>			
+					<a class="col-lg-4 col-md-4 " href="kisan30.pdf" target="_blank">
+        			    <div class="box" >
+              			<span>30</span>
+              			<h4>अप्रैल 2020</h4>
+					   </div>
+       		   </a>
+		
+				</div>
+				
